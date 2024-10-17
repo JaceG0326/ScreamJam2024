1cf25bcefede7b9f19ec75f969fbfe8e79b3f758
Jace Glover <jacemac326@gmail.com>
2024-10-17 00:29:16 -0400

    Reworked movement and added animations

--- Witch Platformer/Scripts/Player/playerMovement.gd
+++ Witch Platformer/Scripts/Player/playerMovement.gd
@@ -1,44 +1,52 @@
 extends CharacterBody2D
 
 # Change to constants later
-@export var speed = 250
-@export var jumpPower = -2000
-@export var accel = 30
-@export var friction = 50
-@export var gravity = 120
+const speed = 175
+const jumpPower = -300
+const decel_on_jump_release = 0.25
+const accel = 0.05
+const friction = 0.05
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var anim = $AnimationPlayer
+@onready var sprite = $Sprite2D
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	var inputDir : Vector2 = input()
+	if not is_on_floor():
+		velocity.y += gravity * delta
 	
-	if inputDir != Vector2.ZERO:
-		accelerate(inputDir)
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jumpPower
+		#velocity.x += speed * 0.5 * velocity.sign().x
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= decel_on_jump_release
+	
+	var inputDir = Input.get_axis("left", "right")
+	if inputDir:
+		velocity.x = move_toward(velocity.x, inputDir * speed, speed * accel)
 	else:
-		add_friction()
-	movement()
-	#jump()
-
-func input() -> Vector2:
-	var inputDir = Vector2.ZERO
+		velocity.x = move_toward(velocity.x, 0, speed * friction)
 	
-	inputDir.x = Input.get_axis("left", "right")
-	inputDir = inputDir.normalized()
-	return inputDir;
-
-func accelerate(dir):
-	velocity = velocity.move_toward(speed * dir, accel)
-
-func add_friction():
-	velocity = velocity.move_toward(Vector2.ZERO, friction)
-
-func movement():
+	animation_handler()
 	move_and_slide()
 
-func jump():
-	if is_on_floor():
-		if Input.is_action_just_pressed("jump"):
-			velocity.y = jumpPower
-	else:
-		velocity.y += gravity
+func animation_handler():
+		if velocity == Vector2.ZERO:
+			anim.play("idle")
+		elif is_on_floor():
+			if velocity.x > 0:
+				anim.play("run")
+				sprite.flip_h = false
+				sprite.offset.x = 24
+			elif velocity.x < 0:
+				anim.play("run")
+				sprite.flip_h = true
+				sprite.offset.x = -24
+		else:
+			#play jumping/falling animations
+			pass
